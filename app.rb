cff2b3c8ff7823cca938ee87c63465d10ec2fd27
Marissa Zhong <mwz2101@columbia.edu>
2018-07-14 11:34:43 +0000

    automatically backed up by learn

---
+++ app.rb
@@ -0,0 +1,24 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+  get '/' do
+    erb :root
+  end
+
+  get '/new' do
+    erb :'pirates/new'
+  end
+
+  post '/pirates' do
+    @pirate = Pirate.new(params[:pirate])
+
+    params[:pirate][:ships].each do |ship|
+      Ship.new(ship)
+    end
+
+    @ships = Ship.all
+    erb :'pirates/show'
+  end
+
+end
